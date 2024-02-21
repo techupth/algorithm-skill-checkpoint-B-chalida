12aaed451fb8d46dcc886eab857743ebb66e0519
bread <chalida.bread@gmail.com>
2024-02-21 14:08:24 +0700

    fix: edit code for max-characters

--- max-characters/index.mjs
+++ max-characters/index.mjs
@@ -6,4 +6,30 @@
 // getMaxCharacters("abcccccccd") === "c"
 // getMaxCharacters("apple 1231111") === "1"
 
-export const getMaxCharacters = (str) => {};
+export const getMaxCharacters = (str) => {
+  let characterMap = {};
+  let maxCharacter = "";
+  let maxCount = 0;
+
+  for (let character of str) {
+    if (characterMap[character]) {
+      characterMap[character]++;
+    } else {
+      characterMap[character] = 1;
+    }
+  }
+
+  for (let character in characterMap) {
+    if (characterMap[character] > count) {
+      maxCount = characterMap[character];
+      maxCharacter = character;
+    }
+    return maxCharacter;
+  }
+
+  const characters1 = "abcccccccd";
+  const characters2 = "apple 1231111";
+
+  console.log(getMaxCharacters(characters1));
+  console.log(getMaxCharacters(characters2));
+};
