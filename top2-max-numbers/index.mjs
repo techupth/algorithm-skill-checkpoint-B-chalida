877afd07a90265615847e6ad9934b673015a21fb
bread <chalida.bread@gmail.com>
2024-02-22 12:54:58 +0700

    fix:fix code for top 2 max numbers

--- top2-max-numbers/index.mjs
+++ top2-max-numbers/index.mjs
@@ -8,4 +8,29 @@
 // getTop2MaxNumbers([-10, -40, -20, -2, -9, -19]) === [-2, -9]
 // getTop2MaxNumbers([10, 40, 20, 2, 40, 9, 19]) === [40, 20]
 
-export const getTop2MaxNumbers = (numbers) => {};
+export const getTop2MaxNumbers = (numbers) => {
+  const uniqueNums = [...new Set(numbers)];
+  let maxNums1 = -Infinity;
+  let maxNums2 = -Infinity;
+
+  for (let i = 0; i < uniqueNums.length; i++) {
+    if (uniqueNums[i] > maxNums1) {
+      maxNums2 = maxNums1;
+      maxNums1 = uniqueNums[i];
+    } else if (uniqueNums[i] > maxNums2) {
+      maxNums2 = uniqueNums[i];
+    }
+  }
+  return [maxNums1, maxNums2];
+};
+
+console.log(getTop2MaxNumbers([-10, -40, -20, -2, -9, -19]));
+console.log(getTop2MaxNumbers([10, 40, 20, 2, 40, 9, 19]));
+
+// export const getTop2MaxNumbers = (numbers) => {
+//   const uniqueNums = [...new Set(numbers)];
+//   const sortedNums = uniqueNums.sort((a, b) => b - a);
+//   return sortedNums.slice(0, 2);
+// };
+// console.log(getTop2MaxNumbers([-10, -40, -20, -2, -9, -19]));
+// console.log(getTop2MaxNumbers([10, 40, 20, 2, 40, 9, 19]));
